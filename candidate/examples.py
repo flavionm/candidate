0cc23e50a1e223220a8efc8a153564a9c732221e
Flávio Sousa <fn_maia@outlook.com>
2020-11-01 21:55:13 -0300

    Add comparative to pure WAT

---
+++ candidate/examples.py
@@ -0,0 +1,35 @@
+'''Tests both annotation methods'''
+import annotator
+import candidate
+
+
+def _examples():
+    _test('Armstrong mon lading')
+    _test('Barak Obama iram bombing', 15)
+    _test('Socialism vs communism', 5)
+
+
+def _test(query, tries=0):
+    _test_custom(query, tries)
+    print()
+    _test_wat(query)
+    print()
+    print()
+
+
+def _test_custom(query, tries):
+    if tries > 0:
+        result = candidate.annotate_query(query, tries)
+    else:
+        result = candidate.annotate_query(query)
+    candidate.print_annotations(result)
+
+
+def _test_wat(query):
+    wat_annotations = annotator.wat_entity_linking(query)
+    for note in wat_annotations:
+        print(f'{note["spot"]}: https://en.wikipedia.org/wiki/{note["wiki_title"]}')
+
+
+if __name__ == "__main__":
+    _examples()
